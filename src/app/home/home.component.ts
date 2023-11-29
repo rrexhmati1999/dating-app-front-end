f28f879d20b366b08b161ff1b46b7e51f73a9f10
rrexhmati1999 <ronaldorexhmati@gmail.com>
2023-11-30 00:31:03 +0100

    End of section 5

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,20 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit{
   registerMode = false;
-  users: any;
 
-  constructor(private http: HttpClient) {}
+  constructor() {}
 
   ngOnInit(): void {
-    this.getUsers();
   }
 
   registerToggle() {
     this.registerMode = !this.registerMode;
   }
 
-  getUsers() {
-    this.http.get('https://localhost:7104/api/Users').subscribe(users => this.users = users);
+  cancelRegisterMode(event: boolean) {
+    this.registerMode = event;
   }
 
 }
