4b6c95a011e86dd8979642bf9f149fc16c84500b
rrexhmati1999 <ronaldorexhmati@gmail.com>
2023-12-01 00:07:42 +0100

    Fixed toastr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { MemberListComponent } from './members/member-list/member-list/member-li
 import { MemberDetailComponent } from './members/member-detail/member-detail/member-detail.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,12 @@ import { MessagesComponent } from './messages/messages.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    //BrowserAnimationModule,
+    FormsModule,
+    //BsDropdownModule.forRoot(),
+    ToastrModule.forRoot({
+      positionClass: 'toast-bottom-right'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
